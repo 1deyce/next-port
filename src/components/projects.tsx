d39d887b88f49c2dda7ace287485451f89a24f13
1deyce <124916049+1deyce@users.noreply.github.com>
2024-09-12 13:50:49 +0200

    Updates

--- src/components/projects.tsx
+++ src/components/projects.tsx
@@ -1,111 +1,87 @@
 import React from "react";
+import Image from "next/image";
+import Img1 from "../public/bankapp.png";
+import Img2 from "../public/blogapp.png";
+import Img3 from "../public/ecotrack.png";
 
 const Projects = () => {
     return (
-        <section className="w-[800px]">
-            <h1 className="flex justify-center items-center text-center text-4xl uppercase my-10 text-white">
-                My Work
-            </h1>
-            <div className="flex justify-center items-center gap-12">
-                <div className="max-w-sm p-4 border border-gray-200 rounded shadow animate-pulse md:p-6 dark:border-gray-400">
-                    <div className="flex items-center justify-center h-48 mb-4 rounded-md">
-                        <img
-                            src="/bankapp.png"
-                            width={250}
-                            height={100}
-                            alt=""
+        <section className="w-[900px] mt-10 py-10 relative">
+            <div className="w-full max-w-2xl lg:max-w-7xl px-6 lg:px-8 mx-auto">
+            <h1 className="font-manrope font-medium text-4xl text-white uppercase mb-10 text-center">Projects</h1>
+            <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-8">
+                <div className="flex items-center flex-col gap-8 w-full group">
+                    <div className="block">
+                        <Image 
+                            className="rounded-3xl object-cover" 
+                            src={Img1}
+                            alt="Project Achievements of Sketch"   
                         />
                     </div>
-                    <div className="h-2.5 bg-gray-200 rounded-full dark:bg-gray-400 w-48 mb-4"></div>
-                    <div className="h-2 bg-gray-200 rounded-full dark:bg-gray-400 mb-2.5"></div>
-                    <div className="h-2 bg-gray-200 rounded-full dark:bg-gray-400 mb-2.5"></div>
-                    <div className="h-2 bg-gray-200 rounded-full dark:bg-gray-400"></div>
-                    <div className="flex items-center mt-4">
-                        <svg
-                            viewBox="0 0 20 20"
-                            fill="currentColor"
-                            xmlns="http://www.w3.org/2000/svg"
-                            aria-hidden="true"
-                            className="w-10 h-10 me-3 text-gray-200 dark:text-gray-400"
-                        >
-                            <path d="M10 0a10 10 0 1 0 10 10A10.011 10.011 0 0 0 10 0Zm0 5a3 3 0 1 1 0 6 3 3 0 0 1 0-6Zm0 13a8.949 8.949 0 0 1-4.951-1.488A3.987 3.987 0 0 1 9 13h2a3.987 3.987 0 0 1 3.951 3.512A8.949 8.949 0 0 1 10 18Z"></path>
-                        </svg>
-                        <div>
-                            <div className="h-2.5 bg-gray-200 rounded-full dark:bg-gray-400 w-32 mb-2"></div>
-                            <div className="w-48 h-2 bg-gray-200 rounded-full dark:bg-gray-400"></div>
+                    <div className="flex items-center justify-between max-w-[406px] lg:max-w-full w-full lg:px-0">
+                        <div className="block">
+                            <h4 className="text-2xl font-manrope font-semibold text-white mb-1">Bank App</h4>
+                            <p className="font-medium text-sm text-gray-400">Python, Flask, HTML Templates</p>
                         </div>
+                        <button
+                            className=" border border-white py-2 px-3.5 rounded-full transition-all duration-300 group-hover:bg-black group-hover:border-[#007ced]">
+                            <svg className="stroke-white transition-all duration-300 group-hover:stroke-[#007ced]"
+                                xmlns="http://www.w3.org/2000/svg" width="17" height="16" viewBox="0 0 17 16"
+                                fill="none">
+                                <path
+                                    d="M9.62553 4L13.6664 8.0409M13.6664 8.0409L9.62553 12.0818M13.6664 8.0409L1.6665 8.0409"
+                                    stroke="" stroke-width="1.6" stroke-linecap="round" />
+                            </svg>
+                        </button>
                     </div>
-                    <span className="sr-only">Loading...</span>
                 </div>
-                <div className="max-w-sm p-4 border border-gray-200 rounded shadow animate-pulse md:p-6 dark:border-gray-400">
-                    <div className="flex items-center justify-center h-48 mb-4 bg-gray-300 rounded dark:bg-gray-400">
-                        <svg
-                            viewBox="0 0 16 20"
-                            fill="currentColor"
-                            xmlns="http://www.w3.org/2000/svg"
-                            aria-hidden="true"
-                            className="w-10 h-10 text-gray-200 dark:text-gray-600"
-                        >
-                            <path d="M14.066 0H7v5a2 2 0 0 1-2 2H0v11a1.97 1.97 0 0 0 1.934 2h12.132A1.97 1.97 0 0 0 16 18V2a1.97 1.97 0 0 0-1.934-2ZM10.5 6a1.5 1.5 0 1 1 0 2.999A1.5 1.5 0 0 1 10.5 6Zm2.221 10.515a1 1 0 0 1-.858.485h-8a1 1 0 0 1-.9-1.43L5.6 10.039a.978.978 0 0 1 .936-.57 1 1 0 0 1 .9.632l1.181 2.981.541-1a.945.945 0 0 1 .883-.522 1 1 0 0 1 .879.529l1.832 3.438a1 1 0 0 1-.031.988Z"></path>
-                            <path d="M5 5V.13a2.96 2.96 0 0 0-1.293.749L.879 3.707A2.98 2.98 0 0 0 .13 5H5Z"></path>
-                        </svg>
+                <div className="group flex items-center flex-col gap-8 w-full">
+                    <div className="block">
+                        <Image className="rounded-3xl object-cover" src={Img2} alt="Project Achievements of Figma" />
                     </div>
-                    <div className="h-2.5 bg-gray-200 rounded-full dark:bg-gray-400 w-48 mb-4"></div>
-                    <div className="h-2 bg-gray-200 rounded-full dark:bg-gray-400 mb-2.5"></div>
-                    <div className="h-2 bg-gray-200 rounded-full dark:bg-gray-400 mb-2.5"></div>
-                    <div className="h-2 bg-gray-200 rounded-full dark:bg-gray-400"></div>
-                    <div className="flex items-center mt-4">
-                        <svg
-                            viewBox="0 0 20 20"
-                            fill="currentColor"
-                            xmlns="http://www.w3.org/2000/svg"
-                            aria-hidden="true"
-                            className="w-10 h-10 me-3 text-gray-200 dark:text-gray-400"
-                        >
-                            <path d="M10 0a10 10 0 1 0 10 10A10.011 10.011 0 0 0 10 0Zm0 5a3 3 0 1 1 0 6 3 3 0 0 1 0-6Zm0 13a8.949 8.949 0 0 1-4.951-1.488A3.987 3.987 0 0 1 9 13h2a3.987 3.987 0 0 1 3.951 3.512A8.949 8.949 0 0 1 10 18Z"></path>
-                        </svg>
-                        <div>
-                            <div className="h-2.5 bg-gray-200 rounded-full dark:bg-gray-400 w-32 mb-2"></div>
-                            <div className="w-48 h-2 bg-gray-200 rounded-full dark:bg-gray-400"></div>
+                    <div className="flex items-center justify-between max-w-[406px] lg:max-w-full w-full lg:px-0">
+                        <div className="block">
+                            <h4 className="text-2xl font-manrope font-semibold text-white mb-1">Blog App</h4>
+                            <p className="font-medium text-lg text-gray-400">Next.js, GraphQL, Tailwind CSS
+                            </p>
                         </div>
+                        <button
+                            className=" border border-white py-2 px-3.5 rounded-full transition-all duration-300 group-hover:bg-black group-hover:border-[#007ced]">
+                            <svg className="stroke-white transition-all duration-300 group-hover:stroke-[#007ced]"
+                                xmlns="http://www.w3.org/2000/svg" width="17" height="16" viewBox="0 0 17 16"
+                                fill="none">
+                                <path
+                                    d="M9.62553 4L13.6664 8.0409M13.6664 8.0409L9.62553 12.0818M13.6664 8.0409L1.6665 8.0409"
+                                    stroke="" stroke-width="1.6" stroke-linecap="round" />
+                            </svg>
+                        </button>
                     </div>
-                    <span className="sr-only">Loading...</span>
                 </div>
-                <div className="max-w-sm p-4 border border-gray-200 rounded shadow animate-pulse md:p-6 dark:border-gray-400">
-                    <div className="flex items-center justify-center h-48 mb-4 bg-gray-300 rounded dark:bg-gray-400">
-                        <svg
-                            viewBox="0 0 16 20"
-                            fill="currentColor"
-                            xmlns="http://www.w3.org/2000/svg"
-                            aria-hidden="true"
-                            className="w-10 h-10 text-gray-200 dark:text-gray-600"
-                        >
-                            <path d="M14.066 0H7v5a2 2 0 0 1-2 2H0v11a1.97 1.97 0 0 0 1.934 2h12.132A1.97 1.97 0 0 0 16 18V2a1.97 1.97 0 0 0-1.934-2ZM10.5 6a1.5 1.5 0 1 1 0 2.999A1.5 1.5 0 0 1 10.5 6Zm2.221 10.515a1 1 0 0 1-.858.485h-8a1 1 0 0 1-.9-1.43L5.6 10.039a.978.978 0 0 1 .936-.57 1 1 0 0 1 .9.632l1.181 2.981.541-1a.945.945 0 0 1 .883-.522 1 1 0 0 1 .879.529l1.832 3.438a1 1 0 0 1-.031.988Z"></path>
-                            <path d="M5 5V.13a2.96 2.96 0 0 0-1.293.749L.879 3.707A2.98 2.98 0 0 0 .13 5H5Z"></path>
-                        </svg>
+                <div
+                    className="group flex items-center flex-col gap-8 w-full md:w-1/2 mx-auto md:col-span-2 lg:col-span-1 lg:w-full">
+                    <div className="block">
+                        <Image className="rounded-3xl object-cover" src={Img3} alt="Project Achievements of Frame.io" />
                     </div>
-                    <div className="h-2.5 bg-gray-200 rounded-full dark:bg-gray-400 w-48 mb-4"></div>
-                    <div className="h-2 bg-gray-200 rounded-full dark:bg-gray-400 mb-2.5"></div>
-                    <div className="h-2 bg-gray-200 rounded-full dark:bg-gray-400 mb-2.5"></div>
-                    <div className="h-2 bg-gray-200 rounded-full dark:bg-gray-400"></div>
-                    <div className="flex items-center mt-4">
-                        <svg
-                            viewBox="0 0 20 20"
-                            fill="currentColor"
-                            xmlns="http://www.w3.org/2000/svg"
-                            aria-hidden="true"
-                            className="w-10 h-10 me-3 text-gray-200 dark:text-gray-400"
-                        >
-                            <path d="M10 0a10 10 0 1 0 10 10A10.011 10.011 0 0 0 10 0Zm0 5a3 3 0 1 1 0 6 3 3 0 0 1 0-6Zm0 13a8.949 8.949 0 0 1-4.951-1.488A3.987 3.987 0 0 1 9 13h2a3.987 3.987 0 0 1 3.951 3.512A8.949 8.949 0 0 1 10 18Z"></path>
-                        </svg>
-                        <div>
-                            <div className="h-2.5 bg-gray-200 rounded-full dark:bg-gray-400 w-32 mb-2"></div>
-                            <div className="w-48 h-2 bg-gray-200 rounded-full dark:bg-gray-400"></div>
+                    <div className="flex items-center justify-between max-w-[406px] lg:max-w-full w-full lg:px-0">
+                        <div className="block">
+                            <h4 className="text-2xl font-manrope font-semibold text-white mb-1">Waste Management App</h4>
+                            <p className="font-medium text-lg text-gray-400">MERN, Tailwind CSS
+                            </p>
                         </div>
+                        <button
+                            className=" border border-white py-2 px-3.5 rounded-full transition-all duration-300 group-hover:bg-black group-hover:border-[#007ced]">
+                            <svg className="stroke-white transition-all duration-300 group-hover:stroke-[#007ced]"
+                                xmlns="http://www.w3.org/2000/svg" width="17" height="16" viewBox="0 0 17 16"
+                                fill="none">
+                                <path
+                                    d="M9.62553 4L13.6664 8.0409M13.6664 8.0409L9.62553 12.0818M13.6664 8.0409L1.6665 8.0409"
+                                    stroke="" stroke-width="1.6" stroke-linecap="round" />
+                            </svg>
+                        </button>
                     </div>
-                    <span className="sr-only">Loading...</span>
                 </div>
             </div>
+        </div>
         </section>
     );
 };
