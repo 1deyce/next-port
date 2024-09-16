55a372c61408a53025f1353be2ce71af6b00bced
1deyce <124916049+1deyce@users.noreply.github.com>
2024-09-16 12:11:43 +0200

    Experience Update

--- src/components/experience.tsx
+++ src/components/experience.tsx
@@ -1,4 +1,12 @@
 import React from "react";
+import Image from "next/image";
+
+import uni from "../public/uwc.jpeg";
+import sirius from "../public/sirius.jpeg";
+import young from "../public/younglings.jpeg";
+import absa from "../public/absa.jpeg";
+
+import { RiHtml5Fill, RiCss3Fill, RiJavascriptFill,  } from "react-icons/ri";
 
 const Experience = () => {
     return (
@@ -8,47 +16,13 @@ const Experience = () => {
             </h1>
             <ul className="timeline timeline-snap-icon max-md:timeline-compact timeline-vertical">
                 <li>
-                    <div className="timeline-middle">
-                        <svg
-                            xmlns="http://www.w3.org/2000/svg"
-                            viewBox="0 0 20 20"
-                            fill="currentColor"
-                            className="h-5 w-5"
-                        >
-                            <path
-                                fillRule="evenodd"
-                                d="M10 18a8 8 0 100-16 8 8 0 000 16zm3.857-9.809a.75.75 0 00-1.214-.882l-3.483 4.79-1.88-1.88a.75.75 0 10-1.06 1.061l2.5 2.5a.75.75 0 001.137-.089l4-5.5z"
-                                clipRule="evenodd"
-                            />
-                        </svg>
-                    </div>
-                    <div className="timeline-start mb-10 md:text-end">
-                        <time className="font-mono italic">2014 - 2018</time>
-                        <div className="text-lg font-black pb-1 text-[#007ced]">
-                            Norman Henshilwood High School
-                        </div>
-                    </div>
-                    <hr />
-                </li>
-                <li>
                     <hr />
                     <div className="timeline-middle">
-                        <svg
-                            xmlns="http://www.w3.org/2000/svg"
-                            viewBox="0 0 20 20"
-                            fill="currentColor"
-                            className="h-5 w-5"
-                        >
-                            <path
-                                fillRule="evenodd"
-                                d="M10 18a8 8 0 100-16 8 8 0 000 16zm3.857-9.809a.75.75 0 00-1.214-.882l-3.483 4.79-1.88-1.88a.75.75 0 10-1.06 1.061l2.5 2.5a.75.75 0 001.137-.089l4-5.5z"
-                                clipRule="evenodd"
-                            />
-                        </svg>
+                        <Image src={uni} alt="University of the Western Cape" width={50} height={50} className="m-2 rounded-full" />
                     </div>
                     <div className="timeline-end mb-10">
                         <time className="font-mono italic">2019 - 2021</time>
-                        <div className="text-lg font-black text-[#007ced]">
+                        <div className="text-lg font-semibold text-[#007ced]">
                             University of the Western Cape
                         </div>
                         Completed a BA Degree in Sport, Recreation & Exercise
@@ -59,30 +33,19 @@ const Experience = () => {
                 <li>
                     <hr />
                     <div className="timeline-middle">
-                        <svg
-                            xmlns="http://www.w3.org/2000/svg"
-                            viewBox="0 0 20 20"
-                            fill="currentColor"
-                            className="h-5 w-5"
-                        >
-                            <path
-                                fillRule="evenodd"
-                                d="M10 18a8 8 0 100-16 8 8 0 000 16zm3.857-9.809a.75.75 0 00-1.214-.882l-3.483 4.79-1.88-1.88a.75.75 0 10-1.06 1.061l2.5 2.5a.75.75 0 001.137-.089l4-5.5z"
-                                clipRule="evenodd"
-                            />
-                        </svg>
+                        <Image src={sirius} alt="Sirius Support" width={50} height={50} className="m-2 rounded-full" />
                     </div>
                     <div className="timeline-start mb-10 md:text-end">
                         <time className="font-mono italic">
                             Jun 2022 - Dec 2022
                         </time>
-                        <div className="text-lg font-black text-[#007ced]">
+                        <div className="text-lg font-semibold text-[#007ced]">
                             Sirius Support
                         </div>
                         Worked as a Customer Service Agent, providing live text
                         technical support to a diverse client base.
                         <br />
-                        <span className="mt-2 inline-flex items-center rounded-md bg-black px-3 py-2 text-xs font-medium text-gray-300 ring-1 ring-inset ring-gray-500/10">
+                        <span className="mt-2 inline-flex items-center rounded-md bg-slate-800 px-3 py-2 text-xs font-medium text-slate-500 ring-1 ring-inset ring-gray-500/10">
                             Customer Service
                         </span>
                     </div>
@@ -92,44 +55,45 @@ const Experience = () => {
                 <li>
                     <hr />
                     <div className="timeline-middle">
-                        <svg
-                            xmlns="http://www.w3.org/2000/svg"
-                            viewBox="0 0 20 20"
-                            fill="currentColor"
-                            className="h-5 w-5"
-                        >
-                            <path
-                                fillRule="evenodd"
-                                d="M10 18a8 8 0 100-16 8 8 0 000 16zm3.857-9.809a.75.75 0 00-1.214-.882l-3.483 4.79-1.88-1.88a.75.75 0 10-1.06 1.061l2.5 2.5a.75.75 0 001.137-.089l4-5.5z"
-                                clipRule="evenodd"
-                            />
-                        </svg>
+                        <Image src={young} alt="Younglings Africa" width={50} height={50} className="m-2 rounded-full" />
                     </div>
                     <div className="timeline-end mb-10">
                         <time className="font-mono italic">2023</time>
-                        <div className="text-lg font-black text-[#007ced]">
+                        <div className="text-lg font-semibold text-[#007ced]">
                             Younglings Africa
                         </div>
                         Full Stack Developer Intern. Agile Workflow. Contributed
                         to various projects, utilizing the MERN stack to create
                         solutions.
                         <br />
-                        <span className="mt-2 mr-1 inline-flex items-center rounded-md bg-black px-3 py-2 text-xs font-medium text-gray-300 ring-1 ring-inset ring-gray-500/10">
+                        <span className="mt-2 mr-1 inline-flex items-center rounded-md bg-slate-800 px-3 py-2 text-xs font-medium text-slate-500 ring-1 ring-inset ring-gray-500/10">
                             HTML
                         </span>
-                        <span className="mt-2 mr-1 inline-flex items-center rounded-md bg-black px-3 py-2 text-xs font-medium text-gray-300 ring-1 ring-inset ring-gray-500/10">
+                        <span className="mt-2 mr-1 inline-flex items-center rounded-md bg-slate-800 px-3 py-2 text-xs font-medium text-slate-500 ring-1 ring-inset ring-gray-500/10">
                             CSS
                         </span>
-                        <span className="mt-2 mr-1 inline-flex items-center rounded-md bg-black px-3 py-2 text-xs font-medium text-gray-300 ring-1 ring-inset ring-gray-500/10">
+                        <span className="mt-2 mr-1 inline-flex items-center rounded-md bg-slate-800 px-3 py-2 text-xs font-medium text-slate-500 ring-1 ring-inset ring-gray-500/10">
                             JS
                         </span>
-                        <span className="mt-2 mr-1 inline-flex items-center rounded-md bg-black px-3 py-2 text-xs font-medium text-gray-300 ring-1 ring-inset ring-gray-500/10">
+                        <span className="mt-2 mr-1 inline-flex items-center rounded-md bg-slate-800 px-3 py-2 text-xs font-medium text-slate-500 ring-1 ring-inset ring-gray-500/10">
+                            React
+                        </span>
+                        <span className="mt-2 mr-1 inline-flex items-center rounded-md bg-slate-800 px-3 py-2 text-xs font-medium text-slate-500 ring-1 ring-inset ring-gray-500/10">
+                            Node.js
+                        </span>
+                        <span className="mt-2 mr-1 inline-flex items-center rounded-md bg-slate-800 px-3 py-2 text-xs font-medium text-slate-500 ring-1 ring-inset ring-gray-500/10">
+                            MongoDB
+                        </span>
+                        <span className="mt-2 mr-1 inline-flex items-center rounded-md bg-slate-800 px-3 py-2 text-xs font-medium text-slate-500 ring-1 ring-inset ring-gray-500/10">
+                            Express.js
+                        </span>
+                        <span className="mt-2 mr-1 inline-flex items-center rounded-md bg-slate-800 px-3 py-2 text-xs font-medium text-slate-500 ring-1 ring-inset ring-gray-500/10">
                             Figma
                         </span>
-                        <span className="mt-2 mr-1 inline-flex items-center rounded-md bg-black px-3 py-2 text-xs font-medium text-gray-300 ring-1 ring-inset ring-gray-500/10">
+                        <span className="mt-2 mr-1 inline-flex items-center rounded-md bg-slate-800 px-3 py-2 text-xs font-medium text-slate-500 ring-1 ring-inset ring-gray-500/10">
                             Tailwind
                         </span>
-                        <span className="mt-2 inline-flex items-center rounded-md bg-black px-3 py-2 text-xs font-medium text-gray-300 ring-1 ring-inset ring-gray-500/10">
+                        <span className="mt-2 inline-flex items-center rounded-md bg-slate-800 px-3 py-2 text-xs font-medium text-slate-500 ring-1 ring-inset ring-gray-500/10">
                             Firebase
                         </span>
                     </div>
@@ -138,37 +102,26 @@ const Experience = () => {
                 <li>
                     <hr />
                     <div className="timeline-middle">
-                        <svg
-                            xmlns="http://www.w3.org/2000/svg"
-                            viewBox="0 0 20 20"
-                            fill="currentColor"
-                            className="h-5 w-5"
-                        >
-                            <path
-                                fillRule="evenodd"
-                                d="M10 18a8 8 0 100-16 8 8 0 000 16zm3.857-9.809a.75.75 0 00-1.214-.882l-3.483 4.79-1.88-1.88a.75.75 0 10-1.06 1.061l2.5 2.5a.75.75 0 001.137-.089l4-5.5z"
-                                clipRule="evenodd"
-                            />
-                        </svg>
+                        <Image src={absa} alt="Absa" width={50} height={50} className="m-2 rounded-full" />
                     </div>
                     <div className="timeline-start mb-10 md:text-end">
                         <time className="font-mono italic">
                             Jan 2024 - Jul 2024
                         </time>
-                        <div className="text-lg font-black text-[#007ced]">
-                            ABSA Bank
+                        <div className="text-lg font-semibold text-[#007ced]">
+                            ABSA Group
                         </div>
-                        Associate AWS Developer. Developer serverless functions
+                        Associate AWS Developer. Developed serverless functions
                         using AWS Lambda, leveraging Node.js to interact with
                         the AWS SDK and automate AWS-related tasks.
                         <br />
-                        <span className="mt-2 mr-1 inline-flex items-center rounded-md bg-black px-3 py-2 text-xs font-medium text-gray-300 ring-1 ring-inset ring-gray-500/10">
+                        <span className="mt-2 mr-1 inline-flex items-center rounded-md bg-slate-800 px-3 py-2 text-xs font-medium text-slate-500 ring-1 ring-inset ring-gray-500/10">
                             AWS
                         </span>
-                        <span className="mt-2 mr-1 inline-flex items-center rounded-md bg-black px-3 py-2 text-xs font-medium text-gray-300 ring-1 ring-inset ring-gray-500/10">
+                        <span className="mt-2 mr-1 inline-flex items-center rounded-md bg-slate-800 px-3 py-2 text-xs font-medium text-slate-500 ring-1 ring-inset ring-gray-500/10">
                             Amazon Connect
                         </span>
-                        <span className="mt-2 mr-1 inline-flex items-center rounded-md bg-black px-3 py-2 text-xs font-medium text-gray-300 ring-1 ring-inset ring-gray-500/10">
+                        <span className="mt-2 mr-1 inline-flex items-center rounded-md bg-slate-800 px-3 py-2 text-xs font-medium text-slate-500 ring-1 ring-inset ring-gray-500/10">
                             Node.js
                         </span>
                     </div>
