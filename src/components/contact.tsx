71692a4715d940d5cad9acac8221c0ac7a6f6968
1deyce <124916049+1deyce@users.noreply.github.com>
2024-09-16 15:18:35 +0200

    Update contact.tsx

--- src/components/contact.tsx
+++ src/components/contact.tsx
@@ -3,7 +3,7 @@ import { RiMailAddFill } from "react-icons/ri";
 const Contact = () => {
     return (
         <section className="mt-20 max-w-[800px]">
-            <div className="flex flex-col justify-center items-center mx-auto text-center">
+            <div className="flex flex-col justify-center items-center mx-auto text-center p-8">
                 <h1 className="font-medium text-4xl text-white uppercase mb-10 text-center">Get in Touch</h1>
                 <p>I’d love to connect! If you have any questions or would like to discuss opportunities, please send me an email, and I’ll respond as soon as I can. Looking forward to hearing from you!</p>
                 <br />
